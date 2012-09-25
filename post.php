01e3588242798604d843c1275778dc8869a4702d
Nick A <fearfree@cubetown.net>
2012-09-24 21:36:50 -0500

    Clean up error message
    Stopped error message from repeating 1000 times.

--- post.php
+++ post.php
@@ -29,7 +29,9 @@ $query_row = mysql_fetch_array($result);
 #    die('Could not query:' . mysql_error());
 #}
 if(strlen($hash)!=32){
-  echo "MD5 hashes must be 32 characters in length";
+  echo 'MD5 hashes must be 32 characters in length</br>
+<form><input type="button" value="Back" onClick="history.go(-1);return true;"></form></html>';
+  die;
   } else {
   if(($query_row['pass'])==""){
   #do nothing at this time
