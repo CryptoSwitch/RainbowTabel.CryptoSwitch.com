2e7dd66ce08d6658469b115ff951054f454c3905
fearfree <fearfree@cubetown.net>
2012-09-24 19:09:32 -0500

    Update post.php
    Updated to include new multi-database scheme.

--- post.php
+++ post.php
@@ -1,13 +1,28 @@
 <html>
 <?php
+function nextNumber($previous) {
+  if(is_numeric($previous)) {
+    $next = $previous.'a';
+  } else  if(substr($previous,-1) == 'z') {
+    $next = substr($previous,0,-1).'aa';
+  } else {
+    $lastChr = substr($previous,-1);
+    $next = substr($previous,0,-1).++$lastChr;
+  }
+  return $next;
+}
+$hashtag="z";
+
+while(strlen($hashtag)<3){
+
 $hash = $_POST['hash'];
-$link = mysql_connect('IPGOESHERE', 'USERGOESHERE', 'PASSGOESHERE');
+$link = mysql_connect('localhost', 'root', 'y(*32)?={h;IJ28');
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
-mysql_select_db('rainbowtables'); 
+mysql_select_db('rainbowtables');
 
-$result = mysql_query("SELECT pass FROM hashes WHERE hash = '$hash'") or trigger_error(mysql_error());
+$result = mysql_query("SELECT pass FROM Hashes_$hashtag  WHERE hash = '$hash'") or trigger_error(mysql_error());
 
 $query_row = mysql_fetch_array($result);
 #if (!$result) {
@@ -15,13 +30,16 @@ $query_row = mysql_fetch_array($result);
 #}
 if(strlen($hash)!=32){
   echo "MD5 hashes must be 32 characters in length";
-} else {
-  if(($query_row['pass'])==""){
-    echo "Result not yet in database";
   } else {
-  echo($query_row['pass']); // outputs third employee's name
+  if(($query_row['pass'])==""){
+  #do nothing at this time
+  } else {
+    echo($query_row['pass']);
+
+    mysql_close($link);
   }
 }
-mysql_close($link);
+  $hashtag = nextNumber($hashtag);
+}
 ?></br>
 <form><input type="button" value="Back" onClick="history.go(-1);return true;"></form></html>
