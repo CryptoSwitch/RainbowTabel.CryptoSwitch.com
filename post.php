35535f861be48e7c13eebfe1e8728c9384bc182d
FearFree <fearfree@cubetown.net>
2012-09-26 11:08:46 -0500

    Update, and comments

--- post.php
+++ post.php
@@ -1,24 +1,21 @@
 <html>
 <?php
-#Setup function for number increments
-function nextNumber($previous) {
-  if(is_numeric($previous)) {
-    $next = $previous.'a';
-  } else  if(substr($previous,-1) == 'z') {
-    $next = substr($previous,0,-1).'aa';
-  } else {
-    $lastChr = substr($previous,-1);
-    $next = substr($previous,0,-1).++$lastChr;
-  }
-  return $next;
-}
+###Only using letters, this function no longer needed
+#function nextNumber($previous) {
+#  if(is_numeric($previous)) {
+#    $next = $previous.'a';
+#  } else  if(substr($previous,-1) == 'z') {
+#    $next = substr($previous,0,-1).'aa';
+#  } else {
+#    $lastChr = substr($previous,-1);
+#    $next = substr($previous,0,-1).++$lastChr;
+#  }
+#  return $next;
+#}
+$hashtag="zz";
 
-#First database ends in aa, start one letter prior
-$hashtag="z";
-
-
-#Change this if we get databases with >3 trailing alphanumeric chars
-while(strlen($hashtag)<3){
+#Search databases with the table name of Hashes_xxx, increase number later if more tables exist
+while(strlen($hashtag)<4){
 
 #Get data from input, search database
 $hash = $_POST['hash'];
@@ -31,25 +28,34 @@ mysql_select_db('rainbowtables');
 $result = mysql_query("SELECT pass FROM Hashes_$hashtag  WHERE hash = '$hash'") or trigger_error(mysql_error());
 
 $query_row = mysql_fetch_array($result);
-
-#For those who don't understand "Enter hash"
+#if (!$result) {
+#    die('Could not query:' . mysql_error());
+#}
+#Check for correclt length of MD5 hash entered
 if(strlen($hash)!=32){
   echo 'MD5 hashes must be 32 characters in length</br>
 <form><input type="button" value="Back" onClick="history.go(-1);return true;"></form></html>';
   die;
   } else {
-#for the rest
+
+#If no result, do nothing and continue
   if(($query_row['pass'])==""){
   #do nothing at this time
   } else {
+#If result found, show result and stop searching
     echo($query_row['pass']);
+    echo '</br>
+<form><input type="button" value="Back" onClick="history.go(-1);return true;"></form></html>';
 
     mysql_close($link);
+    die;
   }
 }
-
-#advance to next letter and loop
-  $hashtag = nextNumber($hashtag);
+#Advance to next letter and loop
+  $hashtag = ++$hashtag;
 }
-?></br>
+?>
+<!--If no result found, show this-->
+Results not yet in database
+</br>
 <form><input type="button" value="Back" onClick="history.go(-1);return true;"></form></html>
