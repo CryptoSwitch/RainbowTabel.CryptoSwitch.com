17b4fb26afe5b8d6bcb4e3163b916c126c017f37
FearFree <fearfree@cubetown.net>
2012-09-26 11:38:31 -0500

    Comment clean up

--- post.php
+++ post.php
@@ -1,35 +1,22 @@
 <html>
 <?php
-###Only using letters, this function no longer needed
-#function nextNumber($previous) {
-#  if(is_numeric($previous)) {
-#    $next = $previous.'a';
-#  } else  if(substr($previous,-1) == 'z') {
-#    $next = substr($previous,0,-1).'aa';
-#  } else {
-#    $lastChr = substr($previous,-1);
-#    $next = substr($previous,0,-1).++$lastChr;
-#  }
-#  return $next;
-#}
-$hashtag="zz";
 
-#Search databases with the table name of Hashes_xxx, increase number later if more tables exist
-while(strlen($hashtag)<4){
+$hashtag="zz"; //Arbitrary start point, must be </= starting database suffix
 
-#Get data from input, search database
-$hash = $_POST['hash'];
-$link = mysql_connect('localhost', 'mysql_user', 'mysql_pass');
+while(strlen($hashtag)<4){ //Creating loop to continue searching tables until the suffix reaches a length specified.  In this case, once the suffix reaches 4 characters, loop will stop.
+
+$hash = $_POST['hash'];  //Get data entered in text field of query.php
+$link = mysql_connect('localhost', 'mysql_user', 'mysql_pass'); //Connect to database
 if (!$link) {
-    die('Could not connect: ' . mysql_error());
+    die('Could not connect: ' . mysql_error()); //Show error if connection failed.  Will be removed for production
 }
-mysql_select_db('rainbowtables');
+mysql_select_db('rainbowtables');  //Select database
 
-$result = mysql_query("SELECT pass FROM Hashes_$hashtag  WHERE hash = '$hash'") or trigger_error(mysql_error());
+$result = mysql_query("SELECT pass FROM Hashes_$hashtag  WHERE hash = '$hash'") or trigger_error(mysql_error()); //Set query variable with current table in loop
 
-$query_row = mysql_fetch_array($result);
+$query_row = mysql_fetch_array($result);  //Run query
 #if (!$result) {
-#    die('Could not query:' . mysql_error());
+#    die('Could not query:' . mysql_error()); //Show error if query fails, for testing only
 #}
 #Check for correclt length of MD5 hash entered
 if(strlen($hash)!=32){
@@ -47,12 +34,13 @@ if(strlen($hash)!=32){
     echo '</br>
 <form><input type="button" value="Back" onClick="history.go(-1);return true;"></form></html>';
 
-    mysql_close($link);
-    die;
+    mysql_close($link);  //Close MySQL link after result found
+    die;  //If result found, do not continue searching tables
   }
+
 }
-#Advance to next letter and loop
-  $hashtag = ++$hashtag;
+  mysql_close($link); //Close MySQL Connection, should probably remove open and close from loop and only do once
+  $hashtag = ++$hashtag; //Advance suffix to next letter
 }
 ?>
 <!--If no result found, show this-->
