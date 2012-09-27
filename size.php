74543d757497d6492ea6b30ad250b1b829d7a51d
FearFree <fearfree@cubetown.net>
2012-09-26 20:00:58 -0500

    Update size.php to read database instead of filesize

--- size.php
+++ size.php
@@ -1,20 +1,9 @@
 <?php
-	function get_size($size) {
-	$bytes = array('B','KB','MB','GB','TB');
-	  foreach($bytes as $val) {
-	   if($size > 1024){
-	    $size = $size / 1024;
-	   }else{
-	    break;
-	   }
-	  }
-	  return round($size, 2)." ".$val;
-	}
-	
-	 $filename = '/home/gravypod/HashList.txt';
-	 //$size = round(filesize($filename) / 1073741824, 2);
-		$size = get_size(filesize($filename));
-	echo '<p>We currently have ' . $size . ' of Rainbow Tables generated and are still generating!' . "</p>";
 
+$conn = mysql_connect('localhost', 'mysql_user', 'mysql_pass', 1, 65536);
+$res = mysql_query( "select table_name from information_schema.tables where table_schema='rainbowtables'", $conn );
+$size=mysql_num_rows( $res );
+
+        echo '<p>We currently have about ' . $size . ' million rows of Rainbow Tables in the database and are still generating!' . "</p>";  //Each table should have 1 million rows in it.
 
 ?>
